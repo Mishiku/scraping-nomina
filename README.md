60855df6125f088b198811bef891e6c490c6b123
Mishiku <barnizoku@hotmail.com>
2021-06-18 18:47:52 -0500

    Código ahora recursivo, detecta y agrega subsecretarías conforme las va encontrando

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Requerimientos:
-	Node 12
+- Node 12
 
 ## Uso:
+	rm NT<fecha>.json
+	rm-r NT_csvs_<fecha>/
 	node app.js
--- app.js
+++ app.js
@@ -4,26 +4,41 @@ const axios = require('axios').default;
 var sleep = require('sleep');
 const neatCsv = require('neat-csv');
 global.lines = "nope";
+// current timestamp in milliseconds
+let ts = Date.now();
+
+let date_ob = new Date(ts);
+let date = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
+let year = date_ob.getFullYear();
+
+global.fecha = year + "-" + month + "-" + date;
+
+const path = "NT_csvs_" + fecha;
+
+if (!fs.existsSync(path)) {
+    fs.mkdir("NT_csvs_" + fecha, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Directory is created.");
+    });
+}
+
 
 
 async function write2json(response) {
-    fs.appendFile('datillos.json', JSON.stringify(response), function (err) {
+    fs.appendFile('NT' + fecha + '.json', JSON.stringify(response), function (err) {
         if (err) return console.log(err);
-        // console.log(reponse);
-
-        //console.log(response.data['data']['consultaNominaPorRamoPaginado']);
     });
 
 }
 
-async function writecsv(response, r1, r2) {
+async function writecsv(response, r1, r2,name) {
     console.log("writing...");
 
-    await fs.writeFile('csvsOut/' + r1 + '_' + r2 + '.csv', response, function (err) {
+    return await fs.writeFile("NT_csvs_" + fecha + '/' + r1 + '_' + r2 + '_'+name+'.csv', response, function (err) {
         if (err) return console.log(err);
-        // console.log(reponse);
-
-        //console.log(response.data['data']['consultaNominaPorRamoPaginado']);
     });
 
 }
@@ -40,15 +55,18 @@ async function numberOfCalls(ramo, ur) {
         return owo.data['data']['consultaNominaPorRamoTotal']['servPubTotalSector'];
 
     } catch (error) {
-        console.error("reintentando #...");
-        return await numberOfCalls(ramo, ur);
+        if (error.code == "ECONNREFUSED" || error.code == "EHOSTUNREACH") {
+            console.log("reintentando #...");
+            return numberOfCalls(ramo, ur);
+        }
+        console.log(error.code);
     }
 }
 
 async function apicall(ramo, ur) {
 
     try {
-        console.log("trying: " + ramo + " " + ur + " *************** ");
+        // console.log("trying: " + ramo + " " + ur + " *************** ");
 
         const owo = await axios({
             method: 'post',
@@ -56,11 +74,14 @@ async function apicall(ramo, ur) {
             timeout: 10000,
             data: { "operationName": "consultaNominaPorRamoPaginado", "variables": { "_ramo": ramo, "_ur": ur, "_initial": 0, "_size": 10000 }, "query": "query consultaNominaPorRamoPaginado($_ramo: Int!, $_ur: String, $_initial: Int!, $_size: Int!) {\n  consultaNominaPorRamoPaginado(ramo: $_ramo, unidad: $_ur, registroInicio: $_initial, cantidadRegistros: $_size) {\n    responseCode\n    servPubTotalSector\n    listDtoServidorPublicoDto {\n      compensacionGarantizada\n      dependencia\n      nombrePuesto\n      nombres\n      primerApellido\n      segundoApellido\n      sueldoBase\n      ramo\n      idUr\n      __typename\n    }\n    __typename\n  }\n}\n" }
         });
-        return owo.data['data']['consultaNominaPorRamoPaginado'].listDtoServidorPublicoDto[0];
+        return owo.data['data']['consultaNominaPorRamoPaginado'].listDtoServidorPublicoDto;
 
     } catch (error) {
-        console.error("reintentando datos...");
-        return await apicall(ramo, ur);
+        if (error.code == "ECONNREFUSED" || error.code == "EHOSTUNREACH") {
+            console.log("reintentando llamada...");
+            return apicall(ramo, ur);
+        }
+        console.log(error.code);
     }
 
 }
@@ -72,109 +93,81 @@ async function csvcall(ramo, ur) {
         const owo = await axios({
             method: 'get',
             url: 'https://dgti-ejc-ftp.k8s.funcionpublica.gob.mx/APF/' + ramo + '_' + ur + '.csv',
-            timeout: 600000
+            timeout: 120000
         });
         return owo.data;
 
     } catch (error) {
-        console.error("reintentando datos...");
-        return await csvcall(ramo, ur);
+        if (error.code == "ECONNREFUSED" || error.code == "EHOSTUNREACH") {
+            console.log("reintentando...");
+            return csvcall(ramo, ur);
+        }
+        console.log(error.code);
     }
 
 }
-async function checa(r1, r2) {
-    console.log("contando...");
 
-    const readline = require('readline');
-    const fs = require('fs');
 
-    var file = 'csvsOut/' + r1 + '_' + r2 + '.csv';
-    var linesCount = 0;
-    var rl = fs.createReadStream(file);
-    // rl.on('line', function(line){
-    //         console.log("contando...");
-            
-    //         rl.pause();
-    //         linesCount++; // on each linebreak, add +1 to 'linesCount'
-    //         console.log(linesCount);
+async function obtainSectors() {
+    try {
+        console.log("Obteniendo ramos...");
 
-        
-    // });
-    // rl.on('close', async => {
-    //     console.log(linesCount);
-    //     if (linesCount > 10) {
-    //         lines = "pass";
-    //     }
-        var rl = readline.createInterface({
-            input: await fs.createReadStream(file),
-            output: process.stdout,
-            terminal: false
+        const owo = await axios({
+            method: 'get',
+            url: 'https://nominatransparente.rhnet.gob.mx/assets/sectores.json',
+            timeout: 5000
         });
-        await rl.on('line', async (line)=> {
-            try{
-                rl.pause();
-                linesCount++;
-            }
-            finally{
-                rl.resume();
-            }
-            linesCount++; // on each linebreak, add +1 to 'linesCount'
-        });
-        rl.on('close', function () {
-            console.log(linesCount);
-            if(linesCount>10)
-            {
-                lines="pass";
-            }
-        });
-    
+        return owo.data;
+
+    } catch (error) {
+        console.log(error);
+        return obtainSectors();
+    }
 }
 
+async function obtainEntities(sect) {
+    try {
+        const owo = await axios({
+            method: 'post',
+            url: 'https://entes.apps.funcionpublica.gob.mx/',
+            timeout: 10000,
+            data: { "operationName": "obtenerEntes", "variables": { "ramo": sect }, "query": "query obtenerEntes($ramo: Int!) {\n  obtenerEntes(filtro: {ramo: $ramo, nivelGobierno: FEDERAL}) {\n    id\n    unidadResponsable\n    nombreCorto\n    enteDesc\n    __typename\n  }\n}\n" }
+        });
+        return owo.data['data']['obtenerEntes'];
 
-async function watefoc() {
-    var stream = await fs.createReadStream("ramos.csv")
-        .pipe(parse({ delimiter: '~' }))
-        .on('data', async (r) => {
-            try {
-                stream.pause();
-                //sleep.sleep(5);
-                const cycles = await numberOfCalls(r[0], r[1]);
-                console.log(r[2] + ": " + cycles);
-                if (cycles < 10000) {
-                    const uwu = await apicall(r[0], r[1]);
-                    const yay = await write2json(uwu);
-                }
-                else {
-                    const exec = require("child_process").execSync;
-                    lines = "nope";
-                    var todobien = 0;
-                    while (todobien == 0) {
-                        switch (lines) {
-                            case "nope":
-                                // some code
-                                console.log(lines);
+    } catch (error) {
+        console.log(error);
+        return obtainEntities(sect);
+    }
 
-                                const uwu = await csvcall(r[0], r[1]);
-                                const yay = await writecsv(uwu, r[0], r[1]);
-                                await new Promise(r => setTimeout(r, 1000));
-                                const yny = await checa(r[0], r[1]);
-                                await new Promise(r => setTimeout(r, 1000));
-                                
-                                break;
-                            default:
-                                console.log("lines = " + lines);
-                                console.log(typeof lines);
-
-
-                                todobien = 1;
-                                break;
-                        }
-                    }
-                }
-            } finally {
-                stream.resume();
-            }
-        })
 }
-watefoc()
-// dame()
+
+async function main() {
+    const uwu = await obtainSectors();
+    var indices = Object.keys(uwu);
+    for (var i = 0; i < indices.length; i++) {
+        const sect = uwu[indices[i]].id;
+        console.log('\x1b[36m%s\x1b[0m', "RAMO: " + uwu[indices[i]].name);
+        const ents = await obtainEntities(sect);
+
+        var indexEnts = Object.keys(ents);
+        for (var j = 0; j < indexEnts.length; j++) {
+            const cycles = await numberOfCalls(sect, ents[indexEnts[j]].unidadResponsable);
+            console.log(sect + " " + ents[indexEnts[j]].unidadResponsable + " " + ents[indexEnts[j]].nombreCorto + " " + cycles);
+            if (cycles < 10000 && cycles > 0) {
+                const callinfo = await apicall(sect, ents[indexEnts[j]].unidadResponsable);
+                // console.log(callinfo[0]);
+                const yay = await write2json(callinfo);
+            }
+            else {
+                if (cycles > 0) {
+                    const csvinfo = await csvcall(sect, ents[indexEnts[j]].unidadResponsable);
+                    await writecsv(csvinfo, sect, ents[indexEnts[j]].unidadResponsable,ents[indexEnts[j]].nombreCorto);
+                }
+            }
+
+        }
+    }
+}
+
+main()
